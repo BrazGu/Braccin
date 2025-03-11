dcf7da691d27284f826590cf24baf668942600ea
Ruan Faccin <ruanfaccin347@gmail.com>
2025-03-11 20:04:41 -0300

    [FIX] Adjustment

--- calc/src/main/java/com/braccin/calc/service/ValidadorService.java
+++ calc/src/main/java/com/braccin/calc/service/ValidadorService.java
@@ -17,6 +17,7 @@ public class ValidadorService {
         }
         return false;
     }
+
     private boolean validarCPF(String cpf) {
         boolean hasDot = cpf.contains(".");
         boolean valid;
